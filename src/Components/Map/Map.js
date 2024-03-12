0c255dc9d2c93cc1f4dd1dd75d068db0ddc0d40f
Samuel Burchfield <sam@Samuels-MacBook-Pro.local>
2024-03-11 21:26:37 -0400

    fixing map component

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
 
 const containerStyle = {
-  width: '400px',
+  width: '100%',
   height: '400px'
 };
 
 const center = {
   lat: 36.021556510744276,
-  lng: -84.24699525867142
+  lng: -84.24699525867142,
 };
 
 function Map() {
@@ -18,16 +18,15 @@ function Map() {
   })
 
   const [map, setMap] = React.useState(null)
+  const zoom = 16
 
   const onLoad = React.useCallback(function callback(map) {
-    // This is just an example of getting and using the map instance!!! don't just blindly copy!
-    const bounds = new window.google.maps.LatLngBounds(center);
-    map.fitBounds(bounds);
+    map.setZoom(zoom)
 
     setMap(map)
-  }, [])
+  }, [zoom])
 
-  const onUnmount = React.useCallback(function callback(map) {
+  const onUnmount = React.useCallback(function callback() {
     setMap(null)
   }, [])
 
@@ -35,7 +34,7 @@ function Map() {
       <GoogleMap
         mapContainerStyle={containerStyle}
         center={center}
-        zoom={16}
+        zoom={zoom}
         onLoad={onLoad}
         onUnmount={onUnmount}
       >
