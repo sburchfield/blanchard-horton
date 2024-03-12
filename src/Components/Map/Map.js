1796ac1a86002f690db58458a729186e630c2428
Samuel Burchfield <sam@Samuels-MacBook-Pro.local>
2024-03-11 20:39:12 -0400

    adding fb icon and updating key

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,12 +1,48 @@
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+import React from 'react'
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
 
-const Map = withScriptjs(withGoogleMap((props) =>
-  <GoogleMap
-    defaultZoom={16}
-    defaultCenter={{ lat: 36.021556510744276, lng: -84.24699525867142 }}
-  >
-    {props.isMarkerShown && <Marker position={{ lat: 36.021556510744276, lng: -84.24699525867142 }} />}
-  </GoogleMap>
-))
+const containerStyle = {
+  width: '400px',
+  height: '400px'
+};
 
-  export default Map;
+const center = {
+  lat: 36.021556510744276,
+  lng: -84.24699525867142
+};
+
+function Map() {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: "AIzaSyDaQqIpQRYE6u89Emp4QquOnweJtC8abp0"
+  })
+
+  const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback(function callback(map) {
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  return isLoaded ? (
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={16}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+      >
+        { <Marker position={{ lat: 36.021556510744276, lng: -84.24699525867142 }} />}
+        <></>
+      </GoogleMap>
+  ) : <></>
+}
+
+export default React.memo(Map)
